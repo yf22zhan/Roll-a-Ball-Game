d997836c18496603b2bbc7f03411d4c8fbec3d40
yf22zhan <yifanzhang1124@gmail.com>
2016-03-14 13:56:04 -0400

    Upload
    v1.0_20160314

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+    public Text winText;
+
+    Rigidbody rb;
+    int count;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit(); 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (SystemInfo.deviceType == DeviceType.Desktop)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddForce(movement * speed);
+        }
+        else
+        {
+            float moveHorizontal = Input.acceleration.x;
+            float moveVertical = Input.acceleration.y;
+            Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            rb.AddForce(movement * speed);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count;
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
